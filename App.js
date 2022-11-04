c27138c1b1e6f7edf805a0c14e8393306c76fd00
VeelQ <77607763+VeelQ@users.noreply.github.com>
2022-11-04 17:45:25 +0100

    Lab4 horizontal view

--- App.js
+++ App.js
@@ -1,148 +1,201 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React , { Component} from 'react';
+import {
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+    Dimensions,
+} from 'react-native';
 
- import React , { Component} from 'react';
- //import type {Node} from 'react';
- import {
-   SafeAreaView,
-   ScrollView,
-   StatusBar,
-   StyleSheet,
-   Text,
-   useColorScheme,
-   TouchableOpacity,
-   View,
-   Button,
-   Flexbox,
-   
- } from 'react-native';
- 
- import {
-   Colors,
-   DebugInstructions,
-   Header,
-   LearnMoreLinks,
-   ReloadInstructions,
- } from 'react-native/Libraries/NewAppScreen';
- 
- /* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
-  * LTI update could not be added via codemod */
- 
- 
- export default class App extends Component {
- constructor(){
-   super()
-   this.state = {
-     resultText: "",
-     calculationText: ""
-     
-   }
-   this.operations = ['AC','+','-','*','/']
-  }
-  calculateResult(){
-   const text = this.state.resultText
-   this.setState({
-     calculationText: eval(text)
-   })
-   this.setState({
-     resultText: ''
-   })
- 
-  }
-  Validate(){
-   const text = this.state.resultText
-   switch(text.slice(-1)){
-     case '+':  
-     case '-': 
-     case '*': 
-     case '/':
-       return false
-   }
-   return true
-  }
- buttonPressed(text){
-   
- 
-   if(text == '='){
-     
-     return this.Validate() && this.calculateResult()
-   }
-   this.setState({
-     resultText: this.state.resultText+text
-   })
- }
- operate(op){
-   switch(op){
-     case 'AC':
-       this.setState({
-         resultText: '',
-         calculationText: ''
-       })
-       break
-     case '+':  
-     case '-': 
-     case '*': 
-     case '/':
-       const lastchar = this.state.resultText.split('').pop()
-       if(this.operations.indexOf(lastchar) > 0) return
-       if(this.state.resultText == "" ) return
-       this.setState({
-         resultText: this.state.resultText + op
-       })
- 
-       
-   }
-   
- 
- }
- render() {
-   
- 
-  let rows = []
-  let nums = [[1,2,3],[4,5,6],[7,8,9],['.',0,'=']]
-  let row = []
-  for(let i = 0;i < 4;i++){
-   row = []
-   for(let j = 0;j < 3;j++){
-     row.push(<TouchableOpacity onPress={() =>  this.buttonPressed(nums[i][j])} style={styles.btn}>
-       <Text style={styles.btntext}>{nums[i][j]}</Text>
-     </TouchableOpacity>) 
-   }
-   rows.push(<View style={[styles.row ]}>{row}</View>)
-  }
+const window = Dimensions.get("window");
+const screen = Dimensions.get("screen");
+
+
+export default class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+            resultText: "",
+            calculationText: "",
+            dimensions: {
+              window,
+              screen
+            }
+        }
+        this.operations = ['AC','+','-','*','/']
+        this.operations1 = ["√","eˣ","ln","e","π"]
+        this.operations2 = ["x!","10ˣ","log₁₀","x²","x³"]
+        this.operations3 = ["+/-","%"]
+    }
+
+    calculateResult(){
+        const text = this.state.resultText
+        this.setState({
+            calculationText: eval(text)
+        })
+        this.setState({resultText: ''})
+    }
+
+    Validate(){
+        const text = this.state.resultText
+        switch(text.slice(-1)){
+            case '+':  
+            case '-': 
+            case '*': 
+            case '/':
+                return false
+        }
+        return true
+    }
+
+    buttonPressed(text){
+        if(text == '='){
+            return this.Validate() && this.calculateResult()
+        }
+        this.setState({
+            resultText: this.state.resultText+text
+        })
+    }
+
+    operate(op){
+        switch(op){
+            case 'AC':
+                this.setState({
+                    resultText: '',
+                    calculationText: ''
+                })
+                break
+            case '+':  
+            case '-': 
+            case '*': 
+            case '/':
+                const lastchar = this.state.resultText.split('').pop()
+                if(this.operations.indexOf(lastchar) > 0) return
+                if(this.state.resultText == "" ) return
+                this.setState({
+                    resultText: this.state.resultText + op
+                })
+                break
+        }
+    }
+
   
- 
-  let ops = []
-  for(let i = 0;i < 5;i++){
-     ops.push(<TouchableOpacity style={styles.btn} onPress={() => this.operate(this.operations[i])}>
-       <Text style={[styles.btntext, styles.white]}>{this.operations[i]}</Text>
-     </TouchableOpacity>) 
-   
-  }
-   return (
-     <View style={styles.container}>
-      <View style={styles.result}>
-       <Text style={styles.resultText}>{this.state.resultText}</Text>
-      </View>
-      <View style={styles.calculation}>
-       <Text style={styles.calculationText}>{this.state.calculationText}</Text>
-      </View>
-      <View style={styles.buttons}>
-       <View style={styles.numbers}>
-         {rows}
-       </View>
-       <View style={styles.operations}>
-         {ops}
-       </View>
-      </View>
-     </View>
-   );
- };
+    onChange = ({ window, screen }) => {
+      this.setState({ dimensions: { window, screen } });
+    };
+  
+    componentDidMount() {
+      this.dimensionsSubscription = Dimensions.addEventListener("change", this.onChange);
+    }
+
+    componentWillUnmount() {
+      this.dimensionsSubscription?.remove();
+    }
+
+    render() {
+        let rows = []
+        let nums = [[1,2,3],[4,5,6],[7,8,9],['.',0,'=']]
+        let row = []
+        let ops = []
+        let altOps1 = []
+        let altOps2 = []
+        let altOps3 = []
+        let altRow = []
+        const { dimensions: { window, screen } } = this.state
+
+        for(let i = 0;i < 4;i++){
+            row = []
+
+            for(let j = 0;j < 3;j++){
+                row.push(<TouchableOpacity onPress={() =>  this.buttonPressed(nums[i][j])} style={styles.btn}>
+                <Text style={styles.btntext}>{nums[i][j]}</Text>
+                </TouchableOpacity>) 
+            }
+            rows.push(<View style={styles.row }>{row}</View>)
+        }
+        for(let i = 0; i < 2; i++){
+          altOps3.push(<TouchableOpacity style={styles.btn}>
+            <Text style={styles.btntext}>{this.operations3[i]}</Text>
+            </TouchableOpacity>) 
+        }
+        altRow.push(<View style={styles.row }>{altOps3}</View>)
+        
+        for(let i = 0;i < 5;i++){
+          ops.push(<TouchableOpacity style={styles.btn} onPress={() => this.operate(this.operations[i])}>
+          <Text style={[styles.btntext, styles.white]}>{this.operations[i]}</Text>
+          </TouchableOpacity>) 
+
+
+          altOps1.push(<TouchableOpacity style={styles.btn}>
+          <Text style={[styles.btntext, styles.white]}>{this.operations1[i]}</Text>
+          </TouchableOpacity>) 
+
+          altOps2.push(<TouchableOpacity style={styles.btn}>
+          <Text style={[styles.btntext, styles.white]}>{this.operations2[i]}</Text>
+          </TouchableOpacity>) 
+        }
+
+        if(window.height > screen.width){
+          return (
+            <View style={styles.container}>
+              <View style={styles.result}>
+                <Text style={styles.resultText}>{this.state.resultText}</Text>
+              </View>
+
+              <View style={styles.calculation}>
+                <Text style={styles.calculationText}>{this.state.calculationText}</Text>
+              </View>
+
+              <View style={styles.buttons}>
+                <View style={styles.numbers}>
+                    {rows}
+                </View>
+                <View style={styles.operations}>
+                    {ops}
+                </View>      
+              </View>
+
+            </View>
+          )
+        }
+        else{
+          return (
+            <View style={styles.container}>
+              <View style={styles.result}>
+                <Text style={styles.resultText}>{this.state.resultText}</Text>
+              </View>
+
+              <View style={styles.calculation}>
+                <Text style={styles.calculationText}>{this.state.calculationText}</Text>
+              </View>
+
+              <View style={styles.buttons}>
+                <View style={styles.operations}>
+                  {altOps1}
+                </View>   
+                <View style={styles.operations}>
+                  {altOps2}
+                </View>  
+
+                <View style={styles.numbers}>
+                  <View style={styles.operations}>
+                    {altRow}
+
+                  </View>
+
+                  {rows}
+                </View>
+                <View style={styles.operations}>
+                  {ops}
+                </View>      
+              </View>
+
+            </View>
+        )
+        }
+
+
+    }
  }
  
  const styles = StyleSheet.create({
